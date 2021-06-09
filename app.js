65b8f191d3279aa46473ac168a0d9f822bcdade1
Steven Chen <schen196@my.centennialcollege.ca>
2021-06-09 02:31:17 -0400

    added connection to mongodb

--- app.js
+++ app.js
@@ -8,10 +8,15 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/authenticationDB",
+{useNewUrlParser: true,  useUnifiedTopology: true });
 
 var indexRouter = require("./routes/index");
 
 var app = express();
+
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
